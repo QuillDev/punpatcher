3d1185c133b1b382ec7f3cefb24e093878825a60
Robert Brunson <rdbrunso@ncsu.edu>
2022-06-03 20:58:23 -0400

    patch test

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import {PunishmentCategory} from "./src/models/Punishments";
+import {PunishmentAction, PunishmentCategory} from "./src/models/Punishments";
 import fetch from 'cross-fetch';
 
 const API_URL = "http://localhost:3000";
@@ -13,6 +13,29 @@ const getCategories = async (): Promise<PunishmentCategory[]> => {
     return categories;
 }
 
+const modifyCategory = async (category: PunishmentCategory) => {
+    await fetch(API_URL + "/v1/punishment-category/" + category.name, {
+        method: "PATCH",
+        body: JSON.stringify(category),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    });
+
+    console.log(`Attempted to patch ${category.short}`)
+}
+
 ( async () => {
+    const category: PunishmentCategory = {
+        short: "test",
+        name: "test_thing",
+        deprecated: false,
+        message: "some message",
+        level: 1,
+        scale: PunishmentAction.MUTE,
+        ordinal: 12,
+        displayMaterial: "WHITE_WOOL"
+    };
+    await modifyCategory(category)
     await getCategories();
 })();
