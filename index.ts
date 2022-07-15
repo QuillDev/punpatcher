247f00bb40d76ca7f12a7e1409d90dfea10ac879
Robert Brunson <robert.brunson@ext.superleague.com>
2022-07-15 17:47:30 -0400

    pun cat

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import {patchToCategory, PunishmentCategory, PunishmentCategoryPatch} from "./src/models/Punishments";
+import {PunishmentAction, PunishmentCategory, PunishmentCategoryPatch} from "./src/models/Punishments";
 import fetch from 'cross-fetch';
 import {readFileSync} from "fs";
 import * as console from "console";
@@ -67,19 +67,29 @@ const wipePunishments = async () => {
     const cats = (await getCategories());
     const names = cats.map((it) => it.short);
 
+    await createCategory({
+        deprecated: false,
+        displayMaterial: "FEATHER",
+        level: 1,
+        message: "test_2",
+        name: "test_2",
+        ordinal: 0,
+        scale: PunishmentAction.MUTE,
+        short: "test_2"
+    });
     // await wipePunishments();
 
-    // load the patches from the file
-    const patches: PunishmentCategoryPatch[] = loadFromFile();
-
-    // try to load all patches from the file
-    for(const patch of patches){
-        // if we have not created this punishment, create it
-        if(!names.includes(patch.short)){
-            await createCategory(patchToCategory(patch)).then(console.info);
-            continue;
-        }
-
-        await modifyCategory(patch).then(console.info)
-    }
+    // // load the patches from the file
+    // const patches: PunishmentCategoryPatch[] = loadFromFile();
+    //
+    // // try to load all patches from the file
+    // for(const patch of patches){
+    //     // if we have not created this punishment, create it
+    //     if(!names.includes(patch.short)){
+    //         await createCategory(patchToCategory(patch)).then(console.info);
+    //         continue;
+    //     }
+    //
+    //     await modifyCategory(patch).then(console.info)
+    // }
 })();
