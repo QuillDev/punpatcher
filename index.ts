3130dff3692625fcbf64d1ea9a2150ca77bd829a
Robert Brunson <robert.brunson@ext.superleague.com>
2022-07-16 12:05:36 -0400

    apply new patch:

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import {PunishmentAction, PunishmentCategory, PunishmentCategoryPatch} from "./src/models/Punishments";
+import {patchToCategory, PunishmentAction, PunishmentCategory, PunishmentCategoryPatch} from "./src/models/Punishments";
 import fetch from 'cross-fetch';
 import {readFileSync} from "fs";
 import * as console from "console";
 
 const API_URL = "http://localhost:3000";
 
-const loadFromFile = (): PunishmentCategoryPatch[] => {
-    const puns: PunishmentCategoryPatch[] = JSON.parse(readFileSync('patch.json', 'utf-8'));
+const loadFromFile = (fileName: string): PunishmentCategoryPatch[] => {
+    const puns: PunishmentCategoryPatch[] = JSON.parse(readFileSync(fileName, 'utf-8'));
     return puns;
 }
 
@@ -67,29 +67,11 @@ const wipePunishments = async () => {
     const cats = (await getCategories());
     const names = cats.map((it) => it.short);
 
-    await createCategory({
-        deprecated: false,
-        displayMaterial: "FEATHER",
-        level: 1,
-        message: "test_2",
-        name: "test_2",
-        ordinal: 0,
-        scale: PunishmentAction.MUTE,
-        short: "test_2"
-    });
-    // await wipePunishments();
+    // load the patches from the file
+    const patches: PunishmentCategoryPatch[] = loadFromFile('patch2.json');
 
-    // // load the patches from the file
-    // const patches: PunishmentCategoryPatch[] = loadFromFile();
-    //
-    // // try to load all patches from the file
-    // for(const patch of patches){
-    //     // if we have not created this punishment, create it
-    //     if(!names.includes(patch.short)){
-    //         await createCategory(patchToCategory(patch)).then(console.info);
-    //         continue;
-    //     }
-    //
-    //     await modifyCategory(patch).then(console.info)
-    // }
+    // try to load all patches from the file
+    for(const patch of patches){
+        await modifyCategory(patch).then(console.info)
+    }
 })();
